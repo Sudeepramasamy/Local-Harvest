d56dbc0bc2dd42b4797de0f3e0fed716793e4fc2
Sudeepramasamy <161289975+Sudeepramasamy@users.noreply.github.com>
2025-03-19 11:19:25 +0530

    Update wsgi.py

--- api_project/api_project/wsgi.py
+++ api_project/api_project/wsgi.py
@@ -3,8 +3,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_project.settings')
 
-# Run migrations before application starts
-from django.core.management import execute_from_command_line
-execute_from_command_line(['manage.py', 'migrate'])
+
 
 application = get_wsgi_application()
