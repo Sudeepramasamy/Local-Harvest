944394b53464618ae34781ebe2275f7938fbae4c
Sudeep Ramasamy <sudeepramasamy@example.com>
2025-05-15 11:45:34 +0530

    add units

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -131,8 +131,8 @@ const AddProduct = () => {
     <option value="ml">Milliliters (ml)</option>
     <option value="pcs">Pieces (pcs)</option>
     <option value="dozen">Dozen</option>
-    <option value="dozen">Bunch</option>
-    <option value="dozen">Bottle</option>
+    <option value="bunch">Bunch</option>
+    <option value="bottle">Bottle</option>
   </select>
 </div>
 
